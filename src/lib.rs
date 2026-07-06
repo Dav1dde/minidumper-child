6d63fe56570370434c6d5b9f8639f681b199e0a3
David Herberth <github@dav1d.de>
2026-07-06 12:26:38 +0200

    fix: always create the socket in the crashes dir

--- src/lib.rs
+++ src/lib.rs
@@ -112,6 +112,8 @@ impl MinidumperChild {
             panic!("You should set one of 'on_minidump' or 'on_message'");
         }
 
+        std::fs::create_dir_all(&self.crashes_dir)?;
+
         let server_socket = std::env::args()
             .find(|arg| arg.starts_with(&self.server_arg))
             .and_then(|arg| arg.split('=').next_back().map(|arg| arg.to_string()));
@@ -133,7 +135,7 @@ impl MinidumperChild {
         } else {
             // We use a unique socket name because we don't share the crash reporter
             // processes between different instances of the app.
-            let socket_name = make_socket_name(uuid::Uuid::new_v4());
+            let socket_name = make_socket_name(&self.crashes_dir, uuid::Uuid::new_v4());
 
             std::env::current_exe()
                 .and_then(|current_exe| {
@@ -159,16 +161,7 @@ impl MinidumperChild {
     }
 }
 
-pub fn make_socket_name(session_id: uuid::Uuid) -> String {
-    if cfg!(any(target_os = "linux", target_os = "android")) {
-        format!("temp-socket-{}", session_id.simple())
-    } else {
-        // For platforms without abstract uds, put the pipe in the
-        // temporary directory so that the OS can clean it up, rather than
-        // polluting the cwd due to annoying file deletion problems,
-        // particularly on Windows
-        let mut td = std::env::temp_dir();
-        td.push(format!("temp-socket-{}", session_id.simple()));
-        td.to_string_lossy().to_string()
-    }
+pub fn make_socket_name(base_dir: &Path, session_id: uuid::Uuid) -> String {
+    let td = base_dir.join(format!("temp-socket-{}", session_id.simple()));
+    td.to_string_lossy().to_string()
 }
